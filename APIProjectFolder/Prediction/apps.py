fdfc821dcf713664a219b502f3241c9ec73b8da7
Hwang <gymnopedies7@naver.com>
2020-09-12 02:28:07 +0900

    API 완성(RF Prediction)

--- APIProjectFolder/Prediction/apps.py
+++ APIProjectFolder/Prediction/apps.py
@@ -1,5 +1,11 @@
 from django.apps import AppConfig
-
+import pandas as pd
+from joblib import load
+import os
 
 class PredictionConfig(AppConfig):
     name = 'Prediction'
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    CLASSIFIER_FOLDER = os.path.join(BASE_DIR, 'Prediction/classifier/')
+    CLASSIFIER_FILE = os.path.join(CLASSIFIER_FOLDER, "IRISRandomForestClassifier.joblib")
+    classifier = load(CLASSIFIER_FILE)
--- APIProjectFolder/Prediction/views.py
+++ APIProjectFolder/Prediction/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
+import pandas as pd
+from .apps import PredictionConfig
 
 # Create your views here.
 @api_view(['GET', 'POST'])
@@ -18,3 +21,31 @@ def api_add(request):
             sum += data[key]
         response_dict = {"sum": sum}
     return Response(response_dict, status=status.HTTP_201_CREATED)
+
+class Add_Values(APIView):
+    def post(self, request, format=None):
+        sum =0
+        #add the numbers
+        data = request.data
+        for key in data:
+            sum += data[key]
+        response_dict = {"sum" : sum}
+        return Response(response_dict, status=status.HTTP_201_CREATED)    
+
+
+class IRIS_Model_Predict(APIView):
+    def post(self, request, format=None):
+        data = request.data
+        keys = []
+        values = []
+        for key in data:
+            keys.append(key)
+            values.append(data[key])
+        X = pd.Series(values).to_numpy().reshape(1,-1)
+        loaded_classifier = PredictionConfig.classifier
+        y_pred = loaded_classifier.predict(X)
+        y_pred = pd.Series(y_pred)
+        target_map = {0:'setosa', 1:'versicolor', 2:'virginica'}
+        y_pred = y_pred.map(target_map).to_numpy()
+        response_dict = {"Prediced Iris Species": y_pred[0]}
+        return Response(response_dict, status=200)
